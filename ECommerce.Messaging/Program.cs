cebaee93879f2f69dce09ab5b7677221abfc421e
Özlem Karabulut <05170000061@ogrenci.ege.edu.tr>
2023-11-03 16:07:49 +0300

    implement rabbitmqexample

---
+++ ECommerce.Messaging/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://fnbyfelm:ev4qC9VQuazUUOxAMRM8t9BxsMXxYqXQ@chimpanzee.rmq.cloudamqp.com/fnbyfelm");
+using var connection  = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("messageQueue",true,false,false);
+var message = "Try message";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(String.Empty, "messageQueue", null, body);
+
+Console.WriteLine();    
+Console.ReadLine();
+
---
+++ GetMessageExample/Program.cs
@@ -0,0 +1,21 @@
+﻿//ayrı bir projede, mesajı yakalayabiliriz :)
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://fnbyfelm:ev4qC9VQuazUUOxAMRM8t9BxsMXxYqXQ@chimpanzee.rmq.cloudamqp.com/fnbyfelm");
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("messageQueue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("messageQueue", true, consumer);
+
+consumer.Received += Consumer_Received;
+Console.ReadLine();
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+}
