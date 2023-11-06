c5bc0f58eeb36137957134a77b7dbd9509bcd7c9
Özlem Karabulut <05170000061@ogrenci.ege.edu.tr>
2023-11-06 21:04:06 +0300

    add RetailerOperations validator classes.

---
+++ ECommerce.Operation/RetailerOperations/Queries/GetRetailerDetails/GetRetailerByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Operation.RetailerOperations.Cqrs;
+using FluentValidation;
+
+namespace ECommerce.Operation.RetailerOperations.Queries.GetRetailerDetails;
+public class GetRetailerByIdQueryValidator : AbstractValidator<GetRetailerByIdQuery>
+{
+    public GetRetailerByIdQueryValidator()
+    {
+        RuleFor(command => command.Id).NotNull().WithMessage("Retailer Id must be given.");
+        RuleFor(command => command.Id).GreaterThan(0).WithMessage("Retailer Id must be greater than 0.");
+    }
+}
+
