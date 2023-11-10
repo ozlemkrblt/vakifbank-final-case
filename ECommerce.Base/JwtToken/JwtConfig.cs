2b50c090aaf9ac6c2a7db262e990ca25f368512b
Özlem Karabulut <05170000061@ogrenci.ege.edu.tr>
2023-11-10 19:33:23 +0300

    change model id properties

---
+++ ECommerce.Base/JwtToken/JwtConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Base.JwtToken;
+
+    public class JwtConfig
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int AccessTokenExpiration { get; set; }
+    }
+
+
---
+++ ECommerce.Base/Stock/StockStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ECommerce.Base.Stock;
+    public enum StockStatus
+    {
+        OutofStock = 1,
+        InStock = 2,
+        WaitingforRefurbishing = 3,
+        ProductDiscontinued= 4,
+    }
+
